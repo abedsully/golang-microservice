7cd7013d2873ee3815a6b9ddd02711843a8089c7
abedsully <abedsully2909@gmail.com>
2025-01-03 00:24:46 +0700

    feat: added test cases for get all, update, delete

--- api/storer/storer_mysql.go
+++ api/storer/storer_mysql.go
@@ -21,13 +21,13 @@ func (ms *MySQLStorer) CreateProduct(ctx context.Context, p *Product) (*Product,
 	res, err := ms.db.NamedExecContext(ctx, "INSERT INTO products (name, image, category, description, rating, num_reviews, price, count_in_stock) VALUES (:name, :image, :category, :description, :rating, :num_reviews, :price, :count_in_stock)", p)
 
 	if err != nil {
-		return nil, fmt.Errorf("Error inserting product: %w", err)
+		return nil, fmt.Errorf("error inserting product: %w", err)
 	}
 	
 	id, err := res.LastInsertId()
 
 	if err != nil {
-		return nil, fmt.Errorf("Error getting last inserted id: %w", err)
+		return nil, fmt.Errorf("error getting last inserted id: %w", err)
 	}
 
 	p.ID = id
@@ -40,7 +40,7 @@ func (ms *MySQLStorer) GetProduct(ctx context.Context, id int64) (*Product, erro
 
 	err := ms.db.GetContext(ctx, &p, "SELECT * FROM products WHERE id=?", id)
 	if err != nil {
-		return nil, fmt.Errorf("Error getting product: %w", err)
+		return nil, fmt.Errorf("error getting product: %w", err)
 	}
 
 	return &p, nil
@@ -52,27 +52,29 @@ func (ms *MySQLStorer) GetAllProducts(ctx context.Context) ([]*Product, error) {
 	err := ms.db.SelectContext(ctx, &products, "SELECT * FROM products")
 
 	if err != nil {
-		return nil, fmt.Errorf("Error getting list of all products: %w", err)
+		return nil, fmt.Errorf("error getting list of all products: %w", err)
 	}
 
 	return products, nil
 }
 
 func (ms *MySQLStorer) UpdateProduct(ctx context.Context, p *Product) (*Product, error) {
-	_, err := ms.db.ExecContext(ctx, "UPDATE products SET name=:name, image=:image, category=:category, description=:description, rating=:rating, num_reviews=:num_reviews, price=:price, count_in_stock=:count_in_stock WHERE id=:id", p)
+	_, err := ms.db.NamedExecContext(ctx, "UPDATE products SET name=:name, image=:image, category=:category, description=:description, rating=:rating, num_reviews=:num_reviews, price=:price, count_in_stock=:count_in_stock, updated_at=:updated_at WHERE id=:id", p)
 
 	if err != nil {
-		return nil, fmt.Errorf("Error updating product: %w", err)
+		return nil, fmt.Errorf("error updating product: %w", err)
 	}
 
 	return p, nil
 }
 
+
+
 func (ms *MySQLStorer) DeleteProduct(ctx context.Context, id int64) error {
 	_, err := ms.db.ExecContext(ctx, "DELETE FROM products WHERE id=?", id)
 
 	if err != nil {
-		return fmt.Errorf("Error deleting product: %w", err)
+		return fmt.Errorf("error deleting product: %w", err)
 	}
 
 	return nil
--- api/storer/storer_mysql_test.go
+++ api/storer/storer_mysql_test.go
@@ -125,7 +125,6 @@ func TestGetProduct(t *testing.T) {
 				require.NoError(t, err)
 			},
 		},
-		
 	}
 
 	for _, tc := range tcs {
@@ -134,6 +133,167 @@ func TestGetProduct(t *testing.T) {
 				st := NewMySqlStorer(db)
 				tc.test(t, st, mock)
 			})
+		})
+	}
+}
+
+func TestGetAllProducts(t *testing.T) {
+	p := &Product{
+		ID:           1,
+		Name:         "IPhone 16",
+		Image:        "iphone16.png",
+		Category:     "Smartphone",
+		Description:  "Brand new smartphone designed by Apple Inc",
+		Rating:       5,
+		NumReviews:   10,
+		Price:        100.0,
+		CountInStock: 100,
+	}
+
+	tcs := []struct {
+		name string
+		test func(*testing.T, *MySQLStorer, sqlmock.Sqlmock)
+	} {
+		{
+			name: "success",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id", "name", "image", "category", "description", "rating", "num_reviews", "price", "count_in_stock", "created_at", "updated_at"}).AddRow(1, p.Name, p.Image, p.Category, p.Description, p.Rating, p.NumReviews, p.Price, p.CountInStock, p.CreatedAt, p.UpdatedAt)
+				mock.ExpectQuery("SELECT * FROM products").WillReturnRows(rows)
+
+				products, err := st.GetAllProducts(context.Background())
+				require.NoError(t, err)
+				require.Len(t, products, 1)
+				err = mock.ExpectationsWereMet()
+
+				require.NoError(t, err)
+			},
+		},
+		{
+			name: "failed querying products",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				mock.ExpectQuery("SELECT * FROM products").WillReturnError(fmt.Errorf("error querying products"))
+				_, err := st.GetAllProducts(context.Background())
+				require.Error(t, err)
+				err = mock.ExpectationsWereMet()
+				require.NoError(t, err)
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+			st := NewMySqlStorer(db)
+			tc.test(t, st, mock)
+		})
+	}
+}
+
+func TestUpdateProduct(t *testing.T) {
+	p := &Product{
+		ID:           1,
+		Name:         "IPhone 16",
+		Image:        "iphone16.png",
+		Category:     "Smartphone",
+		Description:  "Brand new smartphone designed by Apple Inc",
+		Rating:       5,
+		NumReviews:   10,
+		Price:        100.0,
+		CountInStock: 100,
+	}
+
+	new_p := &Product{
+		ID:           1,
+		Name:         "IPhone 15",
+		Image:        "iphone15.png",
+		Category:     "Smartphone",
+		Description:  "Second latest smartphone designed by Apple Inc",
+		Rating:       4,
+		NumReviews:   8,
+		Price:        90.0,
+		CountInStock: 90,
+	}
+
+	tcs := []struct {
+		name string
+		test func(*testing.T, *MySQLStorer, sqlmock.Sqlmock)
+	}{
+		{
+			name: "success",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				mock.ExpectExec("INSERT INTO products (name, image, category, description, rating, num_reviews, price, count_in_stock) VALUES (?, ?, ?, ?, ?, ?, ?, ?)").WillReturnResult(sqlmock.NewResult(1, 1))
+				cp, err := st.CreateProduct(context.Background(), p)
+				require.NoError(t, err)
+				require.Equal(t, int64(1), cp.ID)
+
+				mock.ExpectExec("UPDATE products SET name=?, image=?, category=?, description=?, rating=?, num_reviews=?, price=?, count_in_stock=?, updated_at=? WHERE id=?").WillReturnResult(sqlmock.NewResult(1, 1))
+
+				up, err := st.UpdateProduct(context.Background(), new_p)
+				require.NoError(t, err)
+				require.Equal(t, int64(1), up.ID)
+				require.Equal(t, new_p.Name, up.Name)
+
+				err = mock.ExpectationsWereMet()
+				require.NoError(t, err)
+			},
+		},
+		{
+			name: "failed updating product",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				mock.ExpectExec("UPDATE products SET name=?, image=?, category=?, description=?, rating=?, num_reviews=?, price=?, count_in_stock=?, updated_at=? WHERE id=?").WillReturnError(fmt.Errorf("error updating product"))
+
+				_, err := st.UpdateProduct(context.Background(), new_p)
+
+				require.Error(t, err)
+				err = mock.ExpectationsWereMet()
+				require.NoError(t, err)
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+				st := NewMySqlStorer(db)
+				tc.test(t, st, mock)
+			})
+		})
+	}
+}
+
+func TestDeleteProduct(t *testing.T) {
+	tcs := []struct {
+		name string
+		test func(*testing.T, *MySQLStorer, sqlmock.Sqlmock)
+	}{
+		{
+			name: "success",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				mock.ExpectExec("DELETE FROM products WHERE id=?").WithArgs(1).WillReturnResult(sqlmock.NewResult(1, 1))
+				err := st.DeleteProduct(context.Background(), 1)
+				require.NoError(t, err)
+
+				err = mock.ExpectationsWereMet()
+				require.NoError(t, err)
+			},
+		},
+		{
+			name: "failed deleting product",
+			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				mock.ExpectExec("DELETE FROM products WHERE id=?").WithArgs(1).WillReturnError(fmt.Errorf("error deleting product"))
+
+				err := st.DeleteProduct(context.Background(), 1)
+				require.Error(t, err)
+
+				err = mock.ExpectationsWereMet()
+				require.NoError(t, err)
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+			st := NewMySqlStorer(db)
+			tc.test(t, st, mock)
 		})
 	}
 }
